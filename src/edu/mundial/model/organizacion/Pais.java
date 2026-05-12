e5e629331acc59eb3755128dbf7a8c1b6678324d
Guillermina Valdes Bahler <guillerminabahler@gmail.com>
2026-05-12 17:41:53 -0300

    Se agrega relacion Seleccion a Pais

--- src/edu/mundial/model/organizacion/Pais.java
+++ src/edu/mundial/model/organizacion/Pais.java
@@ -4,6 +4,7 @@ public class Pais {
     private String nombre;
     private String bandera;
     public Seleccion representa;
+    public Sede pertenece;
 
     public Pais() {
         this.nombre = null;
@@ -15,10 +16,11 @@ public class Pais {
         this.bandera = bandera;
     }
 
-    public Pais(String nombre, String bandera, Seleccion representa) {
+    public Pais(String nombre, String bandera, Seleccion representa, Sede pertenece) {
         this.nombre = nombre;
         this.bandera = bandera;
         this.representa = representa;
+        this.pertenece = pertenece;
     }
 
     public String getNombre() {
@@ -40,5 +42,12 @@ public class Pais {
     }
     public void setRepresenta(Seleccion representa) {
         this.representa = representa;
+    }
+
+    public Sede getPertenece() {
+        return pertenece;
+    }
+    public void setPertenece(Sede pertenece) {
+        this.pertenece = pertenece;
     }
 }
