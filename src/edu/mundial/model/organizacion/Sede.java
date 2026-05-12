3ebac2a2884f090184592fae97cf65b9494bb5d4
Guillermina Valdes Bahler <guillerminabahler@gmail.com>
2026-05-11 21:38:30 -0300

    Se agrega Clase Sede con sus atributos

---
+++ src/edu/mundial/model/organizacion/Sede.java
@@ -0,0 +1,8 @@
+package edu.mundial.model.organizacion;
+
+public class Sede {
+    private String ciudad;
+    private float alturaNivelMar;
+    private String clima;
+    private String zonaHoraria;
+}
