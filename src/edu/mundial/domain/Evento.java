9138b11531b3bd5a30e93128abc5bd5c67de8c83
Vittorio Campodonico <vittoriocampodonico2714@gmail.com>
2026-05-12 17:57:59 -0300

    Se agregaron todos los getters, setters y constructores.

--- src/edu/mundial/domain/Evento.java
+++ src/edu/mundial/domain/Evento.java
@@ -1,7 +1,33 @@
 package edu.mundial.domain;
 import edu.mundial.enums.TipoEvento;
 
-public class Evento {
+public class Evento{
     private TipoEvento tipo;
     private int minuto;
+
+    public Evento(){
+        this.tipo = null;
+        this.minuto = 0;
+    }
+
+    public Evento(TipoEvento tipo, int minuto){
+        this.tipo = tipo;
+        this.minuto = minuto;
+    }
+
+    public TipoEvento getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(TipoEvento tipo){
+        this.tipo = tipo;
+    }
+
+    public int getMinuto(){
+        return minuto;
+    }
+
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
 }
