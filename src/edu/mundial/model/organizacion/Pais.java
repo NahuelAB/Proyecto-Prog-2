493a6b51c38c476a88f555024dfc0ad7b30002d8
Guillermina Valdes Bahler <guillerminabahler@gmail.com>
2026-05-11 22:02:46 -0300

    Agrega a Pais constructores y getters/setters

--- src/edu/mundial/model/organizacion/Pais.java
+++ src/edu/mundial/model/organizacion/Pais.java
@@ -3,4 +3,28 @@ package edu.mundial.model.organizacion;
 public class Pais {
     private String nombre;
     private String bandera;
+
+    public Pais() {
+        this.nombre = null;
+        this.bandera = null;
+    }
+
+    public Pais(String nombre, String bandera) {
+        this.nombre = nombre;
+        this.bandera = bandera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getBandera() {
+        return bandera;
+    }
+    public void setBandera(String bandera) {
+        this.bandera = bandera;
+    }
 }
