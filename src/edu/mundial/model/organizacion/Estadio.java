d68d95983b9f013b25bed4592b9d26480f97937f
Guillermina Valdes Bahler <guillerminabahler@gmail.com>
2026-05-11 21:46:36 -0300

    Agrega a Estadio constructores y Getters/Setters

--- src/edu/mundial/model/organizacion/Estadio.java
+++ src/edu/mundial/model/organizacion/Estadio.java
@@ -3,4 +3,28 @@ package edu.mundial.model.organizacion;
 public class Estadio {
     private String nombre;
     private int capacidad;
+
+    public Estadio() {
+        this.nombre = null;
+        this.capacidad = 0;
+    }
+
+    public Estadio(String nombre, int capacidad) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
 }
