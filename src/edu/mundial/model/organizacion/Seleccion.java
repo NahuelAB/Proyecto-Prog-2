d00009a292bc455c1b9bf214286d9dad58c946a4
Guillermina Valdes Bahler <guillerminabahler@gmail.com>
2026-05-11 22:51:10 -0300

    Agrega a Seleccion constructores y getters/setters

--- src/edu/mundial/model/organizacion/Seleccion.java
+++ src/edu/mundial/model/organizacion/Seleccion.java
@@ -6,4 +6,55 @@ public class Seleccion {
     private String camisetaSecundaria;
     private boolean cabezaGrupo;
     private int rankingFIFA;
+
+    public Seleccion() {
+        this.nombreFederacion = null;
+        this.camisetaPrincipal = null;
+        this.camisetaSecundaria = null;
+        this.cabezaGrupo = false;
+        this.rankingFIFA = 0;
+    }
+
+    public Seleccion(String nombreFederacion, String camisetaPrincipal, String camisetaSecundaria, boolean cabezaGrupo, int rankingFIFA) {
+        this.nombreFederacion = nombreFederacion;
+        this.camisetaPrincipal = camisetaPrincipal;
+        this.camisetaSecundaria = camisetaPrincipal;
+        this.cabezaGrupo = cabezaGrupo;
+        this.rankingFIFA = rankingFIFA;
+    }
+
+    public String getNombreFederacion() {
+        return nombreFederacion;
+    }
+    public void setNombreFederacion(String nombreFederacion) {
+        this.nombreFederacion = nombreFederacion;
+    }
+
+    public String getCamisetaPrincipal() {
+        return camisetaPrincipal;
+    }
+    public void setCamisetaPrincipal(String camisetaPrincipal) {
+        this.camisetaPrincipal = camisetaPrincipal;
+    }
+
+    public String getCamisetaSecundaria() {
+        return camisetaSecundaria;
+    }
+    public void setCamisetaSecundaria(String camisetaSecundaria) {
+        this.camisetaSecundaria = camisetaSecundaria;
+    }
+
+    public boolean getCabezaGrupo() {
+        return cabezaGrupo;
+    }
+    public void setCabezaGrupo(boolean cabezaGrupo) {
+        this.cabezaGrupo = cabezaGrupo;
+    }
+
+    public int getRankingFIFA() {
+        return rankingFIFA;
+    }
+    public void setRankingFIFA(int rankingFIFA) {
+        this.rankingFIFA = rankingFIFA;
+    }
 }
